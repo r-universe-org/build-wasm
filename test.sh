e6ec829fde5c2487558d165071480e3b2e0714fd
Jeroen Ooms <jeroenooms@gmail.com>
2023-12-13 17:30:59 +0100

    add some tests

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite'), '.', repos = 'https://cran.r-project.org')"
-/entrypoint.sh openssl*.tar.gz
-/entrypoint.sh fftw*.tar.gz
-/entrypoint.sh RcppArmadillo*.tar.gz
-/entrypoint.sh sf*.tar.gz
-/entrypoint.sh jsonlite*.tar.gz
+R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools'), '.', repos = 'https://cran.r-project.org')"
+for pkg in *.tar.gz; do
+/entrypoint.sh $pkg
+done

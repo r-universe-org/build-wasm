07b4473d1492a568cb3c450afae00b975a779be0
Jeroen Ooms <jeroenooms@gmail.com>
2023-12-14 12:43:47 +0100

    some more tests

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools'), '.', repos = 'https://cran.r-project.org')"
+R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping'),
+ '.', repos = sprintf('https://%s.r-universe.dev', c('jeroen', 'r-lib', 'cran')))"
 for pkg in *.tar.gz; do
 /entrypoint.sh $pkg
 done

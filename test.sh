94546295314480f08743e9150961f2bb1941dfe3
Jeroen Ooms <jeroenooms@gmail.com>
2023-12-14 17:01:36 +0100

    Test fortran

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping'),
+R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping', 'gam'),
  '.', repos = sprintf('https://%s.r-universe.dev', c('jeroen', 'r-lib', 'cran')))"
 for pkg in *.tar.gz; do
 /entrypoint.sh $pkg

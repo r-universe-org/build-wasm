c0b2fc3715927b0822592b67092b37dcbd8d3664
Jeroen Ooms <jeroenooms@gmail.com>
2023-12-15 22:53:46 +0100

    Use cache dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ set -e
 SOURCEPKG="${1:-$SOURCEPKG}"
 GITHUB_OUTPUT=${GITHUB_OUTPUT:-/dev/stdout}
 
+# Restore package cache dir
+export R_LIBS_USER="${PWD}/pkglib"
+R -e ".libPaths(); installed.packages()[,1:3]"
+
 # Some debugging output
 echo "::group::List available wasm libraries"
 PKG_CONFIG_LIBDIR="${WEBR_ROOT}/wasm/lib/pkgconfig" pkg-config --list-all

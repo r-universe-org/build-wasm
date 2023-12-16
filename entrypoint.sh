0085ddfcfcecf844d699c2fcf059930129e87dd8
Jeroen Ooms <jeroenooms@gmail.com>
2023-12-16 11:57:20 +0100

    collapse output

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,13 @@ set -e
 SOURCEPKG="${1:-$SOURCEPKG}"
 GITHUB_OUTPUT=${GITHUB_OUTPUT:-/dev/stdout}
 
-# Restore package cache dir
+# Use package cache dir
 export R_LIBS_USER="${PWD}/pkglib"
-R -e ".libPaths(); installed.packages()[,1:3]"
 
 # Some debugging output
+echo "::group::List installed R packages"
+R -e ".libPaths(); installed.packages()[,1:3]"
+echo "::endgroup::"
 echo "::group::List available wasm libraries"
 PKG_CONFIG_LIBDIR="${WEBR_ROOT}/wasm/lib/pkgconfig" pkg-config --list-all
 echo "::endgroup::"

4d5cbb77abf71690383c38364e324076de749dd4
Jeroen Ooms <jeroenooms@gmail.com>
2025-06-02 17:28:20 +0200

    Workaround pak bug

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -e
 R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping', 'gam'),
   '.', repos = 'https://cloud.r-project.org')"
 for pkg in *.tar.gz; do
-R -e "pak::pak('deps::$pkg')"
+#broken in pak: R -e "pak::pak('deps::$pkg')"
+R -e "pak::pak('./$pkg')"
 R -e "rwasm::build('./$pkg')"
 done

cc5093ab5682b6b696d271cb21446e67ee0273bc
Jeroen Ooms <jeroenooms@gmail.com>
2025-06-02 16:45:13 +0200

    Need source packages and deps to build wasm

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping', 'gam'),'.')"
+R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping', 'gam'),
+  '.', repos = 'https://cloud.r-project.org')"
 for pkg in *.tar.gz; do
+R -e "pak::pak('deps::$pkg')"
 R -e "rwasm::build('./$pkg')"
 done

a2787355a83780257b6ae01b7991422490e3f3b2
Jeroen Ooms <jeroenooms@gmail.com>
2025-06-18 15:05:20 +0200

    Use another rust example

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-R -e "download.packages(c('RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping', 'gam'),
+R -e "download.packages(c('tomledit', 'RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping', 'gam'),
   '.', repos = 'https://cloud.r-project.org')"
-curl -OL "https://r-rust.r-universe.dev/src/contrib/hellorust_1.2.3.tar.gz"
+#curl -OL "https://r-rust.r-universe.dev/src/contrib/hellorust_1.2.3.tar.gz"
 for pkg in *.tar.gz; do
 #broken in pak: R -e "pak::pak('deps::$pkg')"
 R -e "pak::pak('./$pkg')"

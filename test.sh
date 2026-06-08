a75524f266f9e80b5ced90c0c064a91b8fd18673
Jeroen Ooms <jeroenooms@gmail.com>
2026-06-08 14:32:13 +0200

    Test new protobuf recipe

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-R -e "download.packages(c('tomledit', 'RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping', 'gam'),
+R -e "download.packages(c('protolite', 'RProtoBuf', 'tomledit', 'RcppArmadillo','sf','openssl', 'fftw', 'jsonlite', 'png', 'gdtools', 'ragg', 'systemfonts', 'textshaping', 'gam'),
   '.', repos = 'https://cloud.r-project.org')"
 #curl -OL "https://r-rust.r-universe.dev/src/contrib/hellorust_1.2.3.tar.gz"
 for pkg in *.tar.gz; do
